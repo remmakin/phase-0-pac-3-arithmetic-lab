219128672ae73baa5f1469045855e840b43ebe02
Tolu Akinrinola <remmakins@gmail.com>
2023-03-07 03:08:34 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+  return a + b;
+}    
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function increment (n) {
+  return ++n;
+}
+
+function decrement (n) {
+  return --n;
+}
+
+function makeInt (n) {
+  return (n);
+}
+
+
+function makeInt (string) {
+  return (string);
+}
+
+function preserveDecimal (string) {
+  return (string);
+}
+
+
+function makeInt (string) {
+  return parseInt(string, 10);
+
+}
+
+function preserveDecimal (string) {
+  return parseFloat(string);
+}
